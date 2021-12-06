f0dcb07c7dba63596eadb5581f7496ffba1d5da9
kz-myodoo <kz@myodoo.pl>
2021-12-06 12:37:27 +0100

    [UPT] entrypoint.sh, docker-compose.yml, odoo.conf
    -- fix requirements.txt path in entrypoint.sh
    -- fix entrypoint.sh mount
    -- add smtp hostname to docker-compose.yml and odoo.conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ set -e
 
 # install python packages
 pip3 install pip --upgrade
-pip3 install -r /etc/odoo/requirements.txt
+pip3 install -r /mnt/extra-addons/requirements.txt
 
 # sed -i 's|raise werkzeug.exceptions.BadRequest(msg)|self.jsonrequest = {}|g' /usr/lib/python3/dist-packages/odoo/http.py
 
